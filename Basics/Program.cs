750fb783754724e2f2e48e8c515677661579b640
xyz <vaibhav.makwana@simformsolutions.com>
2026-03-20 17:14:51 +0530

    Completed the basic student functionality

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,1 +1,70 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Practical04;
+
+Console.Write("Enter Student Name: ");
+string name = Console.ReadLine() ?? "Unknown";
+decimal[] marks = new decimal[5];
+
+for (int i = 0; i < 5; i++)
+{
+    while (true)
+    {
+        Console.Write($"Enter marks {i + 1} in range [0–100]: ");
+        if (decimal.TryParse(Console.ReadLine(), out var mark) && mark >= 0 && mark <= 100)
+        {
+            marks[i] = mark;
+            break;
+        }
+        Console.WriteLine("Marks should be in range of 0-100 only. Try again.");
+    }
+}
+
+Console.WriteLine("\nChoose Option:");
+Console.WriteLine("1 - Aggregate");
+Console.WriteLine("2 - MinMark");
+Console.WriteLine("3 - MaximumMark");
+Console.WriteLine("4 - Grade");
+
+var student = new Student
+{
+    Name = name,
+    Marks = marks
+};
+int input;
+while (true)
+{
+    input = int.Parse(Console.ReadLine() ?? "0");
+
+    if (input < 1 || input > 4)
+    {
+        Console.WriteLine("Please Enter correct option in range [1-4].");
+    }
+    else break;
+}
+
+
+var option = (Options)input;
+var studentMarks = student.Marks;
+
+switch (option)
+{
+    case Options.Aggregate:
+        Console.WriteLine($"{student.Name}: {student.CalculateAverageMarks()}");
+        break;
+
+    case Options.MinMark:
+        Console.WriteLine($"Minimum Marks: {studentMarks.Min()}");
+        break;
+
+    case Options.MaximumMark:
+        Console.WriteLine($"Maximum Marks: {studentMarks.Max()}");
+        break;
+
+    case Options.Grade:
+        var avg = student.CalculateAverageMarks();
+        Console.WriteLine($"Grade: {student.CalculateGrade(avg)}");
+        break;
+
+    default:
+        Console.WriteLine("Please Enter Correct Option.");
+        break;
+}
--- Basics/obj/Debug/net10.0/Basics.AssemblyInfo.cs
+++ Basics/obj/Debug/net10.0/Basics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e9f6ef31855348818a0143588fae22ba9bafd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
